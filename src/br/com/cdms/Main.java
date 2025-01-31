94e03dcc649cb1684bbcc733bdd24332455ff33f
Carlos-DMS <carlos.dsanguino@gmail.com>
2025-01-31 13:58:04 -0300

    Encapsulamento

--- src/br/com/cdms/Main.java
+++ src/br/com/cdms/Main.java
@@ -4,21 +4,22 @@ public class Main {
     public static void main(String[] args) {
        //Orientação a objetos
 
-        Pessoa pessoa = new Pessoa();
-
-        pessoa.nome = "Carlos";
-        pessoa.email = "carlos@gmail.com";
-        pessoa.endereco = "rua X, número 123";
+        Pessoa pessoa = new Pessoa("Carlos", "rua X, número 123", "carlos@gmail.com");
 
         pessoa.estudar();
         pessoa.lazer(2);
 
-        Pessoa pessoa2 = new Pessoa();
-
-        pessoa2.nome = "João";
-        pessoa2.email = "joao@gmail.com";
-        pessoa2.endereco = "";
+        Pessoa pessoa2 = new Pessoa("João", "joao@gmail.com");
 
         pessoa2.trabalhar();
+        pessoa2.lazer(3);
+
+        System.out.println();
+
+        pessoa2.setNome("João Silva");
+        System.out.println("Pessoa 2:");
+        System.out.println("nome: " + pessoa2.getNome());
+        System.out.println("endereço: " + pessoa2.getEndereco());
+        System.out.println("email: " + pessoa2.getEmail());
     }
 }
--- src/br/com/cdms/Pessoa.java
+++ src/br/com/cdms/Pessoa.java
@@ -2,11 +2,52 @@ package br.com.cdms;
 
 public class Pessoa {
     //atributos
-    String nome;
-    String endereco;
-    String email;
+    private String nome;
+    private String endereco;
+    private String email;
 
     //construtor(es)
+
+    public Pessoa(String nome, String endereco, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+    }
+
+    //sobrecarga de métodos
+    public Pessoa(String email, String nome) {
+        this.email = email;
+        this.nome = nome;
+        this.endereco = "-";
+    }
+
+    //getters e setters
+
+    public String getNome() {
+        //Posso criar uma auditoria para ver quem está pegando o nome
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        //Posso criar uma auditoria para ver quem está alterando o nome
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     //métodos
     public void trabalhar() {
@@ -24,6 +65,7 @@ public class Pessoa {
      *     <li><strong>0</strong> - Passear</li>
      *     <li><strong>1</strong> - Malhar</li>
      *     <li><strong>2</strong> - Jogar</li>
+     *     <li><strong>3</strong> - Dormir</li>
      *     <li><strong>Outros</strong> - Ficar de boa</li>
      * </ul>
      */
@@ -38,9 +80,16 @@ public class Pessoa {
             case 2:
                 System.out.println(nome + ", jogar");
                 break;
+            case 3:
+                dormir();
+                break;
             default:
                 System.out.println(nome + ", ficar de boa");
                 break;
         }
+    }
+
+    private void dormir() {
+        System.out.println(nome + ", ir dormir");
     }
 }
